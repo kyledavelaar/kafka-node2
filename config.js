8bde05100dda15858cbf012cf2fdd84663811d94
Kyle Davelaar <kyledavelaar@ip-192-168-1-204.us-west-1.compute.internal>
2019-09-03 10:03:15 -0700

    try to make accessible from outside

--- config.js
+++ config.js
@@ -2,6 +2,8 @@ module.exports = {
   kafka_topic: 'cat',
   kafka_topic2: 'dog',
   kafka_server: 'localhost:2181',
+  // kafka_server: 'localhost:9092',
+  // kafka_server: 'host.docker.internal:2181',
   // kafka_server: '192.168.99.100:2181',
   consumer_config:   {
     autoCommit: true,
